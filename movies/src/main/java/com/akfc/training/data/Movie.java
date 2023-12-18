0cea1768e975b9a73daecb9c1eee90db71e4c625
akoudri <akoudri@cenotelie.fr>
2023-12-18 22:58:23 +0100

    Fixes data model definition

--- movies/src/main/java/com/akfc/training/data/Movie.java
+++ movies/src/main/java/com/akfc/training/data/Movie.java
@@ -2,17 +2,28 @@ package com.akfc.training.data;
 
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.data.annotation.AutoPopulated;
+import io.micronaut.data.annotation.GeneratedValue;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
+import lombok.Data;
 
 @Introspected
 @MappedEntity
-public record Movie (
+@Data
+public class Movie {
+
         @Id
-        @AutoPopulated
-        Long id,
-        String title,
-        String director,
-        String genre,
-        String release
-){}
+        @GeneratedValue(GeneratedValue.Type.AUTO)
+        private Long id;
+        private String title;
+        private String director;
+        private String genre;
+        private String release;
+
+        public Movie(String title, String director, String genre, String release) {
+                this.title = title;
+                this.director = director;
+                this.genre = genre;
+                this.release = release;
+        }
+}
