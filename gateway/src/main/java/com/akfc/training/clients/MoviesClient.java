0349507050a32046fc3443a69141c10d3af39588
akoudri <akoudri@cenotelie.fr>
2023-12-26 10:20:44 +0100

    Improves resilience

--- gateway/src/main/java/com/akfc/training/clients/MoviesClient.java
+++ gateway/src/main/java/com/akfc/training/clients/MoviesClient.java
@@ -6,9 +6,11 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.sse.Event;
+import io.micronaut.retry.annotation.Recoverable;
 import reactor.core.publisher.Flux;
 
 @Client("movies")
+@Recoverable
 public interface MoviesClient {
 
     @Get("/movies")
